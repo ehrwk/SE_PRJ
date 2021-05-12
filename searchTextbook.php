f8578ae787dd94dd5458a2743d83a124a3f2b4a7
ehrwk <yundbs2001@naver.com>
2021-05-12 21:16:41 +0900

    Huiseo

--- searchTextbook.php
+++ searchTextbook.php
@@ -1,35 +1,26 @@
 <?php
 
-<<<<<<< HEAD
- $base -> style = table {
-			border-style: solid; border-width: medium;  border-radius: 5em; padding: 0.3em 1.5em; background: rgb(249, 249, 249); margin-left: 5px; border-color: rgb(51, 175, 233);
-=======
+ $base -> style = table { border-style: solid; border-width: medium;  border-radius: 5em; padding: 0.3em 1.5em; background: rgb(249, 249, 249); margin-left: 5px; border-color: rgb(51, 175, 233);}
+ 
+ $base -> style = select{ width: 100px; padding: 0.4em 0.3em; border: none; font-family: inherit;}
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<style>
-		table {
-			border-style: solid; border-width: medium;  border-radius: 5em; padding: 0.3em 1.5em; background: #F9F9F9; margin-left: 5px; border-color: #33afe9;
->>>>>>> 2f7d59bd1c475f4eba71bb9f0459f89257ecaa18
-		}
-
-		select{
-			width: 100px; padding: .4em .3em;
-			border: none;
-			font-family: inherit;
-		}
-
-<<<<<<< HEAD
 		input:focus {outline-color: rgb(51, 175, 233)}
+	</style>
+</head>
+</html>
 
 
  public function searchbox()
  {
  	echo "<form method = "get" action="/save.php">
-	<table>
+	<table id = 'search_box'>
 		<th>
-			<select category>
+			<select id = 'category'>
 				<option>통합검색</option>
 				<option>국내도서</option>
 				<option>국외도서</option>
@@ -37,7 +28,7 @@
 			</select>
 		</th>
 		<th>
-			<input type="text" name="book_name" style="width:20em; height: 2em; border: none;">
+			<input type='text' name='book_name' style='width:20em; height: 2em; border: none;'>
 		</th>
 		<th>
 			<input type="submit" value="검색">
@@ -47,32 +38,3 @@
  }
 
 ?>
-=======
-		input:focus {outline-color: #33afe9}
-
-	</style>
-</head>
-<body>
-	<form method = "get" action="/save.php">
-	<table>
-		<th>
-			<select category>
-				<option>통합검색</option>
-				<option>국내도서</option>
-				<option>국외도서</option>
-				<option>중고도서</option>
-			</select>
-		</th>
-		<th>
-			<input type="text" name="book_name" style="width:20em; height: 2em; border: none;">
-		</th>
-		<th>
-			<input type="submit" value="검색">	/* class="" 버튼이미지 넣기 */
-		</th>
-	</table>
-	</form>
-</body>
-</html>
-
-?>
->>>>>>> 2f7d59bd1c475f4eba71bb9f0459f89257ecaa18
