62c8cc66e835a4ee2e4bc0078efeb542d09da10d
kweonyaeji <myyaeji@naver.com>
2021-05-18 19:09:14 +0900

    Yeji

--- adminlogincheck.php
+++ adminlogincheck.php
@@ -7,19 +7,14 @@ if($userid==""||$userpw==""){
 	echo '<script> alert("아이디 또는 비밀번호를 입력하세요."); history.back(); </script>';
 } else{
 
-<<<<<<< HEAD
 	$mysqli=mysqli_connect('localhost','bitnami','1234','login');
 
 	$jb_sql="SELECT * FROM adminlogin WHERE id='$userid' AND pw='$userpw'";
-=======
-	$mysqli=mysqli_connect('localhost','bitnami','1234','customerInfo');
-
-	$jb_sql="SELECT * FROM member WHERE id='$userid' AND pw='$userpw' AND email='admin@email.com' AND permit=0 AND name='admin'";
->>>>>>> upstream/master
 	$jb_result=mysqli_query($mysqli, $jb_sql);
 
 	if($jb_row=mysqli_fetch_array($jb_result)){
-
+		session_start(); //세션추가코드
+        $_SESSION['id']=$userid;  //세션추가코드
 
 		echo"<script>location.href='adminMainPage.php';</script>";
 	}
