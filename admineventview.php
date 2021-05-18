c339226553e501929cb5da777c5cda251ae47965
JunginO <hardertobreathe@naver.com>
2021-05-18 18:41:41 +0900

    jungin

--- admineventview.php
+++ admineventview.php
@@ -1,44 +1,41 @@
 <?php
-    include_once "adminLayout.inc";
-    $admin = new AdminLayout;
+    error_reporting(E_ALL);
+    ini_set( "display_errors", 1 );
 	$connect =mysqli_connect('localhost','bitnami','1234','event') or die('connection fail');
-	$number = $_GET['number'];
-    $query="SELECT title,content,date,hit,id FROM eventpage WHERE number=$number";
-    $result = $connect->query($query);
-    while($rows=mysqli_fetch_assoc($result)){
-        $title= $rows['title'];
-        $id= $rows['id'];
-        $date= $rows['date'];
-        $content=$rows['content'];
-}
-    $hit="UPDATE eventpage SET hit=hit+1 WHERE number=$number";
-    $connect->query($hit);
+    $number=$_GET['number'];
+    $sql = "SELECT title,content,date,finalDate,hit FROM eventdb WHERE number=$number";
 
-    $admin->content ="
+    $result = $connect->query($sql);
+    $rows=mysqli_fetch_assoc($result);
+
+?>
+
+<div  id="wrap">
     <table>
         <tr>
-            <td class='ontop'>이벤트</td>
+            <td class="ontop">이벤트</td>
         </tr>
-        <table>
-            <tr>
-                <td colspan='4'>$title</td>
-            </tr>
-            <tr>
-                <td>작성자</td>
-                <td>$id</td>
+<table>
+    <tr>
+        <td colspan="4"><?php echo $rows['title']?></td>
+    </tr>
+        <tr>
+            <img src="admineventview_img.php?number=<?php echo $number ?>" / style="max-width: 80%;height: auto;">
+        </tr>
+
+        <tr>
                 <td>소비자조회수</td>
-                <td>조회수</td>
-            </tr>
-            <tr>
-                <td colspan='4'>$content</td>
-            </tr>
-        </table>
-        <div>
-            <button class='btn' onclick='aa.php'>목록으로</button>
-            <button onclick='aa.php'>수정</button>
-            <button onclick='aa.php'>삭제</button>
+                <td><?php echo  $rows['hit']?></td>
+        </tr>
+        <tr>
+            <td colspan="4"><?php echo $rows['content']?></td>
+        </tr>
+</table>
+<div>
+                <button class="btn" onclick="history.back()" >목록으로</button>
+                <button onclick="location.href='./adminevent_modify.php?number=<?=$number?>'">수정</button>
+ 
+                <button onclick="location.href='./adminevent_delete.php?number=<?=$number?>'">삭제</button>
         </div>
-    </table>
-    ";
-    $admin->AdminLayoutMain();
-?>
+        </table>
+</div>
